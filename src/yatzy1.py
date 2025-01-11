f1783535b4d57fb499cc8bbfbc62e6387d682d80
DeEmilyRose <emilyrangel0711@gmail.com>
2025-01-12 00:58:41 +0100

    Refactorización del test y función four_of_a_kind

--- src/yatzy1.py
+++ src/yatzy1.py
@@ -96,16 +96,10 @@ class Yatzy:
         return 0
 
     @staticmethod
-    def four_of_a_kind(_1, _2, d3, d4, d5):
-        tallies = [0] * 6
-        tallies[_1 - 1] += 1
-        tallies[_2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i + 1) * 4
+    def four_of_a_kind(*dice):
+        for die in range(6, 0, -1):
+            if dice.count(die) >= 4:
+                return die * 4
         return 0
 
     @staticmethod
--- test/test_yatzy1.py
+++ test/test_yatzy1.py
@@ -66,8 +66,8 @@ def test_two_Pair():
 
 
 def test_three_of_a_kind():
-    assert 9 == Yatzy().three_of_a_kind(3, 3, 3, 4, 5)
-    assert 15 == Yatzy().three_of_a_kind(5, 3, 5, 4, 5)
+    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 4, 5)
+    assert 15 == Yatzy.three_of_a_kind(5, 3, 5, 4, 5)
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
 
 
