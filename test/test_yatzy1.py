0976fbb77f7360d3c6e208655af9635c4a212c0d
IsolracI <gabrieldiacich@gmail.com>
2025-01-12 19:57:57 +0100

    refactorización de los casos test

--- test/test_yatzy1.py
+++ test/test_yatzy1.py
@@ -4,28 +4,24 @@ from src.yatzy1 import Yatzy
 # available from http://pytest.org/
 
 def test_chance_scores_sum_of_all_dice():
-    expected = 15
-    actual = Yatzy.chance(2, 3, 4, 5, 1)
-    assert expected == actual
+    assert 15 == Yatzy.chance(2, 3, 4, 5, 1)
     assert 16 == Yatzy.chance(3, 3, 4, 5, 1)
 
 
 def test_yatzy_scores_50():
-    expected = 50
-    actual = Yatzy.yatzy(4, 4, 4, 4, 4)
-    assert expected == actual
+    assert 50 == Yatzy.yatzy(4, 4, 4, 4, 4)
     assert 50 == Yatzy.yatzy(6, 6, 6, 6, 6)
     assert 0 == Yatzy.yatzy(6, 6, 6, 6, 3)
 
 
-def test_1s():
-    assert Yatzy.ones(1, 2, 3, 4, 5) == 1
+def test_ones():
+    assert 1 == Yatzy.ones(1, 2, 3, 4, 5)
     assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
     assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
     assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
 
 
-def test_2s():
+def test_twos():
     assert 4 == Yatzy.twos(1, 2, 3, 2, 6)
     assert 10 == Yatzy.twos(2, 2, 2, 2, 2)
 
@@ -35,7 +31,7 @@ def test_threes():
     assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
 
 
-def test_fours_test():
+def test_fours():
     assert 12 == Yatzy.fours(4, 4, 4, 5, 5)
     assert 8 == Yatzy.fours(4, 4, 5, 5, 5)
     assert 4 == Yatzy.fours(4, 5, 5, 5, 5)
@@ -47,7 +43,7 @@ def test_fives():
     assert 20 == Yatzy.fives(4, 5, 5, 5, 5)
 
 
-def test_sixes_test():
+def test_sixes():
     assert 0 == Yatzy.sixes(4, 4, 4, 5, 5)
     assert 6 == Yatzy.sixes(4, 4, 6, 5, 5)
     assert 18 == Yatzy.sixes(6, 5, 6, 6, 5)
@@ -81,12 +77,12 @@ def test_four_of_a_knd():
 def test_smallStraight():
     assert 15 == Yatzy.smallStraight(1, 2, 3, 4, 5)
     assert 15 == Yatzy.smallStraight(2, 3, 4, 5, 1)
-    assert 0 == Yatzy().smallStraight(1, 2, 2, 4, 5)
+    assert 0 == Yatzy.smallStraight(1, 2, 2, 4, 5)
 
 
 def test_largeStraight():
     assert 20 == Yatzy.largeStraight(6, 2, 3, 4, 5)
-    assert 20 == Yatzy().largeStraight(2, 3, 4, 5, 6)
+    assert 20 == Yatzy.largeStraight(2, 3, 4, 5, 6)
     assert 0 == Yatzy.largeStraight(1, 2, 2, 4, 5)
 
 
