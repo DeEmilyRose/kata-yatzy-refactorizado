c7c6e7d0321deb2a24797063527e2a71a45494fb
DeEmilyRose <emilyrangel0711@gmail.com>
2025-01-12 20:01:17 +0100

    Refactorización del test y función smallStraight

--- test/test_yatzy1.py
+++ test/test_yatzy1.py
@@ -81,7 +81,7 @@ def test_four_of_a_knd():
 def test_smallStraight():
     assert 15 == Yatzy.smallStraight(1, 2, 3, 4, 5)
     assert 15 == Yatzy.smallStraight(2, 3, 4, 5, 1)
-    assert 0 == Yatzy().smallStraight(1, 2, 2, 4, 5)
+    assert 0 == Yatzy.smallStraight(1, 2, 2, 4, 5)
 
 
 def test_largeStraight():
